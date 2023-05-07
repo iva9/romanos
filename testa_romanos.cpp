1b3c22a4d0f08857c989838a0cb597066b71e26c
iva9 <ivanovmsantos@hotmail.com>
2023-05-07 01:04:45 -0300

    D de 500

--- testa_romanos.cpp
+++ testa_romanos.cpp
@@ -16,6 +16,8 @@ TEST_CASE("Numeros romanos - algarismos únicos", "[romanos]") {
     REQUIRE(romanos_para_decimal("X") == 10);
 
     REQUIRE(romanos_para_decimal("L") == 50);
+
+    REQUIRE(romanos_para_decimal("D") == 500);
 }
 
 
