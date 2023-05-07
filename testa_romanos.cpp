a57cf2fee68a10f09a691912fc11f6f160f3696c
iva9 <ivanovmsantos@hotmail.com>
2023-05-07 01:20:42 -0300

    Adicionando teste 20

--- testa_romanos.cpp
+++ testa_romanos.cpp
@@ -15,6 +15,8 @@ TEST_CASE("Numeros romanos - algarismos únicos", "[romanos]") {
 
     REQUIRE(romanos_para_decimal("X") == 10);
 
+    REQUIRE(romanos_para_decimal("XX") == 20);
+
     REQUIRE(romanos_para_decimal("L") == 50);
 
     REQUIRE(romanos_para_decimal("D") == 500);
